acd610394473c9558c51d43254ab3af726ccde09
Anette1974 <anette.cedergren@yahoo.se>
2025-05-01 15:44:02 +0200

    Refactor US1 & US2

--- src/steps/steps_navigate_webpage.py
+++ src/steps/steps_navigate_webpage.py
@@ -1,7 +1,7 @@
 
 import re
 from behave import given, when, then
-from playwright.sync_api import Page, expect
+from playwright.sync_api import expect
 
 @given(u'Jag navigerar till webbsidan "Läslistan"')
 def step_given_start(context):
@@ -10,8 +10,7 @@ def step_given_start(context):
 
 @when(u'jag klickar på knappen "Lägg till bok"')
 def step_when_add_book_button(context):
-    button_locator = context.page.get_by_role("button")
-    add_book_button = button_locator.get_by_text(re.compile("Lägg till bok"))
+    add_book_button = context.page.get_by_test_id("add-book")
     add_book_button.click()
     context.add_book_button = add_book_button
 
@@ -21,8 +20,7 @@ def step_then_add_book_disabled(context):
 
 @then(u'jag klickar på knappen "Mina böcker"')
 def step_when_my_books(context):
-    button_locator = context.page.get_by_role("button")
-    my_books_button = button_locator.get_by_text(re.compile("Mina böcker"))
+    my_books_button = context.page.get_by_test_id("favorites")
     my_books_button.click()
     context.my_books_button = my_books_button
 
@@ -32,10 +30,9 @@ def step_then_my_books_disabled(context):
 
 @then(u'jag klickar på knappen "Katalog"')
 def step_then_catalogue(context):
-    button_locator = context.page.get_by_role("button")
-    catalogue_button = button_locator.get_by_text(re.compile("Katalog"))
-    catalogue_button.click()
-    context.catalogue_button = catalogue_button
+    catalog_button = context.page.get_by_test_id("catalog")
+    catalog_button.click()
+    context.catalogue_button = catalog_button
 
 @then(u'ska knappen "Katalog" ändra färg och inte gå att klicka på')
 def step_then_catalogue_disabled(context):
